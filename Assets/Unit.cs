97600c55176b9cdb3541d076221a7da1a51be29d
hcps-mayeswt <hcps-mayeswt@henricostudents.org>
2016-05-06 15:48:26 -0400

    Added files via upload
    Added unity files and a basic unit class for testing.

---
+++ Assets/Unit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+	int Health;
+	int Range;
+	int Damage;
+	int Speed;
+	
+	public Unit(){
+		//Spawn Unit
+	}
+	
+	// Use this for initialization
+	/*void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}*/
+}
